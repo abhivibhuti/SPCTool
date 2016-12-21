7d70c262c26c4297074b57d18781ef6aebdc41b6
AdminS Abhisek Vibhuti <ABV-adminS@wpcorpnet.com>
2016-12-21 19:08:28 +0530

    Changed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,4 +6,7 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  subscriptions = ["VICH","ABV","LRR","TIH"]
+  regions = ["China","India","Brazil","Europe"]
+  plants = ["CN-QHD","IN-DAB","BR-BRS","DK-ASV"]
 }
